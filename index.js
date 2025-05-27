bc6ca54989885712c261a292e5bc35b9b72e0c8d
OkauanDev <teko4278@gmail.com>
2025-05-27 02:33:23 -0300

    finalizando portifolio

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 
 
-
-
 function enviarWhats(event){
     event.preventDefault();
 
@@ -17,3 +15,30 @@ function enviarWhats(event){
     console.log(url)
     window.open(url, '_blank')
 }
+
+
+
+// Funcionalidade do botão de abrir/fechar menu
+document.addEventListener('DOMContentLoaded', () => {
+    const btnAbrir = document.getElementById('btn-menu');
+    const btnFechar = document.getElementById('btn-fechar');
+    const menuMobile = document.getElementById('menu-mobile');
+    const overlay = document.getElementById('overlay');
+
+    btnAbrir.addEventListener('click', () => {
+        menuMobile.classList.add('open-menu');
+        overlay.classList.add('active');
+        btnAbrir.style.display = 'none'; // esconde o botão ☰
+    });
+
+    const fecharMenu = () => {
+        menuMobile.classList.remove('open-menu');
+        overlay.classList.remove('active');
+        btnAbrir.style.display = 'block'; // mostra o botão ☰
+    }
+
+    btnFechar.addEventListener('click', fecharMenu);
+    overlay.addEventListener('click', fecharMenu);
+});
+
+
